7821cc50000ce89eca7dacf09b13817477b7635c
birando <birger1@live.com>
2024-07-21 19:41:28 +0200

    updated with local url capabilities

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,12 +8,22 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/template.html",
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "src/assets", to: "assets" },
+        { from: "src/template.html", to: "template.html" },
+      ],
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(jpg|png|jpeg|gif|svg)$/i,
+        type: "asset/resource",
       },
     ],
   },
